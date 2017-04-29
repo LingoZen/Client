5976f72ee15f931f2ce312e0c7aa90c651435658
Nickolas Freitas <nickolas@nickolasfreitas.com>
2017-04-29 14:27:14 -0400

    create login model interface

---
+++ src/app/interfaces/login-model.interface.ts
@@ -0,0 +1,6 @@
+import {Language} from "../models/language.models";
+
+export interface LoginModel {
+    username: string,
+    password: Language
+}
--- src/app/interfaces/search-query.interface.ts
+++ src/app/interfaces/search-query.interface.ts
@@ -1,6 +1,6 @@
 import {Language} from "../models/language.models";
 
 export interface SearchQuery {
-  searchString: string,
-  language: Language
+    searchString: string,
+    language: Language
 }
